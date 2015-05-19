781f8d43e327bedbada96e664b6c44b2d4820fa2
YingTzu <tzu_0223@hotmail.com>
2015-05-19 13:00:26 +0800

    Framwork 1

---
+++ Cunning Pigs/Assets/Camera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour 
+{
+	private Transform player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 playerPos = player.position;
+		transform.position = playerPos;
+	}
+}
---
+++ Cunning Pigs/Assets/InputContorller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputContorller : MonoBehaviour {
+	public GameObject target;
+	public float moveSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+//		if (Input.GetKey (KeyCode.UpArrow))
+//			target.transform.position.y -= moveSpeed;
+//		if (Input.GetKey (KeyCode.DownArrow))
+//			target.transform.position.y += moveSpeed;
+//		if (Input.GetKey (KeyCode.LeftArrow))
+//			target.transform.position.x-= moveSpeed;
+//		if (Input.GetKey (KeyCode.RightArrow))
+//			target.transform.position.x += moveSpeed;
+
+	}
+}
