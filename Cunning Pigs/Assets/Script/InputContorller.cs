80bbf20e30774384088dcc65876f559d8f72923a
YingTzu <tzu_0223@hotmail.com>
2015-06-30 11:02:43 +0800

    wolf animation

--- Cunning Pigs/Assets/Script/InputContorller.cs
+++ Cunning Pigs/Assets/Script/InputContorller.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class InputContorller : MonoBehaviour {
 	public float moveSpeed = 0.05f;
-
+	public AnimationClip Up;
+	public AnimationClip Down;
+	public AnimationClip Left;
+	public AnimationClip Right;
+	public AnimationClip Idle;
 	// Use this for initialization
 	void Start () {
 	}
@@ -12,25 +16,24 @@ public class InputContorller : MonoBehaviour {
 	void Update () 
 	{
 		Vector2 temp = transform.position;
-		if (Input.GetKey (KeyCode.UpArrow))
-		{
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
 			temp.y += moveSpeed;
 			transform.position = temp;
-		}
-		else if (Input.GetKey (KeyCode.DownArrow))
-		{
+			GetComponent<Animator>().SetInteger ("Type", 4);
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
 			temp.y -= moveSpeed;
 			transform.position = temp;
-		}
-		else if (Input.GetKey (KeyCode.LeftArrow))
-		{
+			GetComponent<Animator>().SetInteger ("Type", 3);
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
 			temp.x -= moveSpeed;
 			transform.position = temp;
-		}
-		else if (Input.GetKey (KeyCode.RightArrow))
-		{
+			GetComponent<Animator>().SetInteger ("Type", 1);
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
 			temp.x += moveSpeed;
 			transform.position = temp;
-		}
+			GetComponent<Animator>().SetInteger ("Type", 2);
+		} else
+			GetComponent<Animator>().SetInteger ("Type", 0);
 	}
 }
--- Cunning Pigs/Assets/Script/PlayerMovement.cs
+++ Cunning Pigs/Assets/Script/PlayerMovement.cs
@@ -3,29 +3,42 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 	public float moveSpeed = 0.5f;
-
+	public AnimationClip Up;
+	public AnimationClip Down;
+	public AnimationClip Left;
+	public AnimationClip Right;
+	public Animator PlayerAnimation;
+	
 	public void moveUp()
 	{
 		Vector2 temp = transform.position;
 		temp.y += moveSpeed;
 		transform.position = temp;
+		//PlayerAnimation.Play (Up.name);
+		PlayerAnimation.SetInteger ("Type", 4);
 	}
 	public void moveDown()
 	{
 		Vector2 temp = transform.position;
 		temp.y -= moveSpeed;
 		transform.position = temp;
+		//PlayerAnimation.Play (Down.name);
+		PlayerAnimation.SetInteger ("Type", 3);
 	}
 	public void moveLeft()
 	{
 		Vector2 temp = transform.position;
 		temp.x -= moveSpeed;
 		transform.position = temp;
+		//PlayerAnimation.Play (Left.name);
+		PlayerAnimation.SetInteger ("Type", 1);
 	}
 	public void moveRight()
 	{
 		Vector2 temp = transform.position;
 		temp.x += moveSpeed;
 		transform.position = temp;
+		//PlayerAnimation.Play (Right.name);
+		PlayerAnimation.SetInteger ("Type", 2);
 	}
 }
