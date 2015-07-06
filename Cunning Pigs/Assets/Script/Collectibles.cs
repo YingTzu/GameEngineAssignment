d5223bf0d81abe2e7bc313af24ae632ad5fc9865
SunShuyi <prettycure_16@hotmail.com>
2015-07-06 14:26:13 +0800

    Collectibles Started

---
+++ Cunning Pigs/Assets/Script/Collectibles.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectibles : MonoBehaviour {
+
+void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.gameObject.tag == "Player") {
+			Destroy (this.gameObject);
+		}
+	}
+}
