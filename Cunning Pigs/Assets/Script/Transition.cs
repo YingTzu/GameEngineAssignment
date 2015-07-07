9550c00da9dacd5f1556cbe54945b706a05fe507
YingTzu <tzu_0223@hotmail.com>
2015-07-07 11:06:45 +0800

    level transition

---
+++ Cunning Pigs/Assets/Script/Transition.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		{
+			Collider[]hitCollides =Physics.OverlapSphere(this.transform.position,0);
+			for(int i = 0; i < hitCollides.Length; i++)
+			{
+			if (hitCollides[i].gameObject.tag =="Player")
+			
+
+					Application.LoadLevel("level 2");
+			}
+
+		}
+
+	}
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.collider.gameObject.tag =="Player")
+			Application.LoadLevel("level 2");
+	}
+}
