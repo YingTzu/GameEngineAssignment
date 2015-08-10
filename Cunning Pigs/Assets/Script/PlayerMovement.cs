f0576d0ae7d8cbd92a8d5fcf192621527e1646ca
YingTzu <tzu_0223@hotmail.com>
2015-08-10 18:44:32 +0800

    new Movement

--- Cunning Pigs/Assets/Script/PlayerMovement.cs
+++ Cunning Pigs/Assets/Script/PlayerMovement.cs
@@ -1,117 +1,112 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-	public float moveSpeed = 0.01f; 
+	public float moveSpeed; 
 
 	private bool walkUp;
 	private bool walkLeft;
 	private bool walkRight;
 	private bool walkDown;
 
-//	public void moveUp()
-//	{
-//		Vector2 temp = transform.position;
-//		temp.y += moveSpeed;
-//		transform.position = temp;
-//		//PlayerAnimation.Play (Up.name);
-//		GetComponent<Animator> ().SetInteger ("Type", 4);
-//	}
-//	public void moveDown()
-//	{
-//		Vector2 temp = transform.position;
-//		temp.y -= moveSpeed;
-//		transform.position = temp;
-//		GetComponent<Animator>().SetInteger ("Type", 3);
-//	}
-//	public void moveLeft()
-//	{
-//		Vector2 temp = transform.position;
-//		temp.x -= moveSpeed;
-//		transform.position = temp;
-//		GetComponent<Animator>().SetInteger ("Type", 1);
-//	}
-//	public void moveRight()
-//	{
-//		Vector2 temp = transform.position;
-//		temp.x += moveSpeed;
-//		transform.position = temp;
-//		GetComponent<Animator>().SetInteger ("Type", 2);
-//	}
-//	void Update () {
-//
-//	}
+	public Button up;
+	public Button down;
+	public Button left;
+	public Button right;
+
+	void Start () {
+		moveSpeed = 4 * Time.deltaTime;
+		up.GetComponent<Image> ().CrossFadeAlpha (0, 1, false);
+		down.GetComponent<Image> ().CrossFadeAlpha (0, 1, false);
+		left.GetComponent<Image> ().CrossFadeAlpha (0, 1, false);
+		right.GetComponent<Image> ().CrossFadeAlpha (0, 1, false);
+	}
 
 	public void Update()
 	{
-		if (walkUp) {
+		int numberOfTouches = Input.touchCount;
+		
+		if(numberOfTouches > 0)
+		{
+			for (int i = 0; i < numberOfTouches; i++)
+			{
+				Touch touch = Input.GetTouch(i);
+				
+				if(touch.phase == TouchPhase.Began)
+				{
+					if(touch.tapCount >= 2)
+					{
+						walkUp = false;
+						walkDown = false;
+						walkLeft = false;
+						walkRight = false;
+					}
+
+				}
+			}
+		}
+
+		characterMove ();
+	}
+
+	void characterMove()
+	{
+		if (walkUp) 
+		{
 			Vector2 temp = transform.position;
-			temp.y += 0.01f;
+			temp.y += moveSpeed;
 			transform.position = temp;
-			//GetComponent<Rigidbody2D> ().AddForce (Vector2.up * moveSpeed);
 			GetComponent<Animator> ().SetInteger ("Type", 4);
 		} else if (walkLeft) {
 			Vector2 temp = transform.position;
-			temp.x -= 0.01f;
+			temp.x -= moveSpeed;
 			transform.position = temp;
-			//GetComponent<Rigidbody2D> ().AddForce (-Vector2.right * moveSpeed);
 			GetComponent<Animator> ().SetInteger ("Type", 1);
 		} else if (walkRight) {
 			Vector2 temp = transform.position;
-					temp.x += 0.01f;
-					transform.position = temp;
-//			GetComponent<Rigidbody2D> ().AddForce (Vector2.right * moveSpeed);
+			temp.x += moveSpeed;
+			transform.position = temp;
 			GetComponent<Animator> ().SetInteger ("Type", 2);
 		} else if (walkDown) {
 			Vector2 temp = transform.position;
-					temp.y -= 0.01f;
-					transform.position = temp;
-//			GetComponent<Rigidbody2D> ().AddForce (-Vector2.up * moveSpeed);
+			temp.y -= moveSpeed;
+			transform.position = temp;
 			GetComponent<Animator> ().SetInteger ("Type", 3);
-		} else {
 		}
+		up.GetComponent<Image> ().CrossFadeAlpha (0, 0.5f, false);
+		down.GetComponent<Image> ().CrossFadeAlpha (0, 0.5f, false);
+		left.GetComponent<Image> ().CrossFadeAlpha (0, 0.5f, false);
+		right.GetComponent<Image> ().CrossFadeAlpha (0, 0.5f, false);
 	}
-	public void PlayerWalkUp(int value){
-		if (value == 1)
-		{
-			walkUp = true;
-		}
-		else
-		{
-			walkUp = false;
-		}
+
+	public void PlayerWalkUp()
+	{
+		walkUp = true;
+		walkDown = false;
+		walkLeft = false;
+		walkRight = false;
 	}
-	
-	public void PlayerWalkLeft(int value){
-		if (value == 1)
-		{
-			walkLeft = true;
-		}
-		else
-		{
-			walkLeft = false;
-		}
+	public void PlayerWalkDown()
+	{
+		walkUp = false;
+		walkDown = true;
+		walkLeft = false;
+		walkRight = false;
 	}
-	
-	public void PlayerWalkRight(int value){
-		if (value == 1)
-		{
-			walkRight = true;
-		}
-		else
-		{
-			walkRight = false;
-		}
+	public void PlayerWalkLeft()
+	{
+		walkUp = false;
+		walkDown = false;
+		walkLeft = true;
+		walkRight = false;
+			
 	}
-	
-	public void PlayerWalkDown(int value){
-		if (value == 1)
-		{
-			walkDown = true;
-		}
-		else
-		{
-			walkDown = false;
-		}
+	public void PlayerWalkRight()
+	{
+		walkUp = false;
+		walkDown = false;
+		walkLeft = false;
+		walkRight = true;
 	}
 }
