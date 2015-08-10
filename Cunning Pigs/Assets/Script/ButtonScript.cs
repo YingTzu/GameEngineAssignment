0bb749fc9b85f8bdb6ee1ec52321be95823b454b
SunShuyi <prettycure_16@hotmail.com>
2015-08-10 19:54:26 +0800

    reset

--- Cunning Pigs/Assets/Script/ButtonScript.cs
+++ Cunning Pigs/Assets/Script/ButtonScript.cs
@@ -1,9 +1,27 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour 
 {
+	public bool resetOptionAppear;
+	public bool yesAppear;
+	public bool noAppear;
+
+	public Image YesButton;
+	public Image NoButton;
+	public Image ResetOptions;
+
+	private GameObject popOut;
+
 	void Start () {
+		popOut = GameObject.Find ("PopOut");
+
+		resetOptionAppear = false;
+
+		ResetOptions.enabled = false;
+		YesButton.enabled = false;
+		NoButton.enabled = false;
 	}
 
 	void Update () {
@@ -32,5 +50,38 @@ public class ButtonScript : MonoBehaviour
 	public void selectLevel3()
 	{
 		Application.LoadLevel("Level 3");
+	}
+
+	public void ResetUserData()
+	{
+		if (resetOptionAppear == false) 
+		{
+			ResetOptions.enabled = true;
+			YesButton.enabled = true;
+			NoButton.enabled = true;
+
+			resetOptionAppear = true;
+		} 
+	}
+
+	public void yesButton()
+	{
+		ResetOptions.enabled = false;
+		YesButton.enabled = false;
+		NoButton.enabled = false;
+		
+		resetOptionAppear = false;
+
+		PlayerPrefs.DeleteAll();
+		PlayerPrefs.SetInt ("Game_hasBeenPlayedBefore", 0);
+	}
+
+	public void noButton()
+	{
+		ResetOptions.enabled = false;
+		YesButton.enabled = false;
+		NoButton.enabled = false;
+
+		resetOptionAppear = false;
 	}
 }
